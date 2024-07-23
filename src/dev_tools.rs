057d949a600a0f87c551c4b393fc17a7a5c1e19d
n0tap <unmarcianitomagico@hotmail.com>
2024-07-23 03:23:35 -0500

    movi egui a devdependencies

--- src/dev_tools.rs
+++ src/dev_tools.rs
@@ -3,10 +3,14 @@
 use bevy::{dev_tools::states::log_transitions, prelude::*};
 
 use crate::screen::Screen;
-use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
+#[cfg(test)]
+use bevy_inspector_egui;
+//#[cfg(feature = "dev")]
+//use bevy_inspector_egui::quick::WorldInspectorPlugin;
+#[cfg(test)]
 pub(super) fn plugin(app: &mut App) {
     // Print state transitions in dev builds
     app.add_systems(Update, log_transitions::<Screen>);
-    app.add_plugins(WorldInspectorPlugin::new());
+    app.add_plugins(bevy_inspector_egui::quick::WorldInspectorPlugin::new());
 }
